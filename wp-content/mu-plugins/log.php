87c626a7c63494e1fa53dc1d04b77948f5e1db18
Presslabs Support <support@presslabs.com>
2017-05-05 13:29:14 +0000

    Merged changes from  on 05.05.2017

--- wp-content/mu-plugins/log.php
+++ wp-content/mu-plugins/log.php
@@ -1,2 +1,9 @@
 <?php
-error_log('Hello world! ' . $_SERVER['REQUEST_URI']);
+add_action( 'init', 'do_sleep' );
+function do_sleep() {
+	global $wpdb;
+	if ( defined( 'DOING_CRON' ) && DOING_CRON ) {
+		error_log( 'doing cron' );
+		$wpdb->query("SELECT SLEEP(3)");
+	}
+}
