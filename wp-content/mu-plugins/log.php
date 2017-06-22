4570eb7dfb2ee16f06b702d5311f9a8686fedb38
Cristian Uibar <cristi@presslabs.com>
2017-06-22 12:25:07 +0300

    PL: Removed log.php content to not load the SQL anymore

--- wp-content/mu-plugins/log.php
+++ wp-content/mu-plugins/log.php
@@ -1,9 +1,9 @@
 <?php
-add_action( 'init', 'do_sleep' );
-function do_sleep() {
-	global $wpdb;
-	if ( defined( 'DOING_CRON' ) && DOING_CRON ) {
-		error_log( 'doing cron' );
-		$wpdb->query("SELECT SLEEP(3)");
-	}
-}
+// add_action( 'init', 'do_sleep' );
+// function do_sleep() {
+// 	global $wpdb;
+// 	if ( defined( 'DOING_CRON' ) && DOING_CRON ) {
+// 		error_log( 'doing cron' );
+// 		$wpdb->query("SELECT SLEEP(3)");
+// 	}
+// }
