3b0a80a2b4f5a1b1681fbf20f8d8bb36ccc50938
Presslabs Support Account <support@presslabs.com>
2017-08-30 11:18:31 +0000

    updated plugin `Yoast SEO` version 5.3.3

--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit6d2f5330bf06ad7dc7e00268b6bf40bf::getLoader();
+return ComposerAutoloaderInitcc1755b1d09f77a063058c1ec45890e6::getLoader();
